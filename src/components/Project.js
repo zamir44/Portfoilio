266c685f96bcd8cbccd44c280dd04619706f1d00
zamir44 <tooradicalxxi@gmail.com>
2021-03-31 02:58:59 +0200

    portfolio finished

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import data from '../data'
 
 const Project = () => {
+    const [items, setItems] = useState(data)
     return (
-        <div>
-            Projects component
-        </div>
+        <>
+            <section className="py-20 px-5 bg-green-800 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
+                {items.map((item) => {
+                    const { id, path, desc } = item
+                    return (
+                        <div key={id}>
+                            <img src={path} alt={desc} className="p-5 bg-green-500 rounded grid grid-cols-1 gap-5" />
+                            <small className="block text-center text-white font-semibold text-lg">{desc}</small>
+                        </div>
+                    )
+                })}
+            </section>
+        </>
     )
 }
 
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Showcase = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <section className="showcase">
+                <div className="overlay flex flex-col items-center justify-center text-white px-5">
+                    <h1 className="text-5xl font-bold text-right mb-10 lg:text-center lg:text-7xl lg:px-56">I create <span className="text-green-500">brand awareness</span> for small businesses</h1>
+
+                <div className="ml-auto lg:ml-0">
+                    <Link to="/hire" className="mr-5 bg-green-500 py-4 px-4 rounded-full font-semibold transition-all hover:bg-green-800">Get started</Link>
+                    <Link to="/projects"  className="mr-5 bg-white text-green-500 py-4 px-4 rounded-full font-semibold transition-all hover:bg-green-800 hover:text-white">Projects</Link>
+                </div>
+                </div>
+
+            </section>
+        </>
     )
 }
 
